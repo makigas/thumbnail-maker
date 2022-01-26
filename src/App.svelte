108e04ea8747d2f684c3fa7b1222a60866d5185d
Dani Rodríguez <dani@danirod.es>
2022-01-26 23:05:07 +0100

    First version of Thumbnailmaker 2.0

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Form from './Form.svelte';
+	import Thumbnail from './Thumbnail.svelte';
+
+	let download = null
+
+	let state = {
+		title: "Tutorial de Git",
+		chapter: "Capítulo 14",
+		url: "https://cdn.makigas.es/playlists/thumbnails/000/000/014/default/git.png?1483739128",
+		category: "java",
+	};
+
+	function handleSubmit() {
+		console.log("pues bueno")
+		console.log({ download })
+		download = Date.now()
+		console.log({ download })
+	}
+
+</script>
+
+<main>
+  <Thumbnail download={download} state={state} />
+  <Form on:submit={handleSubmit} bind:state={state} />
+</main>
+
+<style>
+	main {
+		max-width: 1024px;
+		margin: 0 auto;
+		padding-left: 35px;
+		padding-right: 35px;
+	}
+</style>
---
+++ src/Form.svelte
@@ -0,0 +1,35 @@
+<script>
+	import TextBox from './TextBox.svelte';
+	import Select from './Select.svelte';
+
+  export let state;
+	let choices = [
+		["programming", "Programación"],
+		["skills", "Mejora tus skills"],
+		["gamedev", "Desarrollo de juegos"],
+		["java", "Java"],
+		["live", "Emisiones en directo"],
+		["webdev", "Desarrollo web"],
+	];
+</script>
+
+<form on:submit|preventDefault>
+  <TextBox label="Serie" placeholder="Título de la serie" bind:value={state.title} />
+  <TextBox label="Capítulo" placeholder="Nombre del capítulo" bind:value={state.chapter} />
+  <TextBox label="URL" placeholder="URL de la foto" bind:value={state.url} />
+  <Select label="Categoría" bind:value={state.category} choices={choices} />
+	<button type="submit">Descargar</button>
+</form>
+
+<style>
+button {
+	width: 100%;
+}
+
+@media (min-width: 800px) {
+  button {
+  	margin-left: 20rem;
+		width: calc(100% - 20rem);
+  }
+}
+</style>
