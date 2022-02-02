16bbdc8a66c6a8fe80d56d46705bf716e0db8f3e
danirod <dani@danirod.es>
2022-02-02 21:13:20 +0000

    Deploying to gh-pages from @ makigas/thumbnail-maker@24dd6531247b73b4e22430cef2e1f2342df0c73e 🚀

---
+++ src/TextBox.svelte
@@ -0,0 +1,48 @@
+<script>
+	// TODO: Can Svelte fail if a prop is not given?
+	export let label = "";
+	export let placeholder = label;
+	export let value = null;
+</script>
+
+{#if label}
+<label>
+<span>{label}:</span>
+<input type="text" placeholder={placeholder} bind:value={value} />
+</label>
+{/if}
+
+<style>
+label {
+	display: block;
+	width: 100%;
+}
+
+span {
+  margin-bottom: 0.25rem;
+}
+
+span, input {
+	display: block;
+	width: 100%;
+}
+
+@media (min-width: 800px) {
+	label{
+		display: flex;
+    gap: 1rem;
+    align-items: baseline;
+	}
+	span, input {
+		display: initial;
+	}
+	span {
+		max-width: 19rem;
+		text-align: right;
+	}
+	input {
+		flex: 1;
+	}
+}
+</style>
+
